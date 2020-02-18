aa4091f3fe33381ad766967d137ebd05e4ead742
Larryliu52 <lanzeliu52@gmail.com>
2020-02-17 22:46:23 -0800

    student model part

---
+++ src/main/java/com/csye6225/springsemester2020/model/Student.java
@@ -0,0 +1,12 @@
+package com.csye6225.springsemester2020.model;
+
+import java.util.Map;
+
+public class Student {
+
+    private long studentId;
+    private String firstName;
+    private String lastName;
+    private String imageUrl;
+    private Map<Long, Course> enrolledCourses
+}
